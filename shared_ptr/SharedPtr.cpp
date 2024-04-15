bfbae3f013098ff057e9d7e6e2f4aeb7c9aefa9d
wuhaolei <nell53052@gmail.com>
2024-04-15 19:42:54 +0800

    feat: complete shared ptr

--- shared_ptr/SharedPtr.cpp
+++ shared_ptr/SharedPtr.cpp
@@ -11,6 +11,33 @@ public:
         release();
     }
 
+    // 拷贝构造函数
+    SharedPtr(const SharedPtr &other) : ptr_(other.ptr_), count_(other.count_)
+    {
+        std::cout << "copy constructor" << std::endl;
+        if (count_)
+        {
+            ++(*count_);
+        }
+    }
+
+    // =等于
+    SharedPtr &operator=(const SharedPtr &other)
+    {
+        std::cout << "operator=" << std::endl;
+        if (this != &other)
+        {
+            release(); // 删除旧有数据
+            ptr_ = other.ptr_;
+            count_ = other.count_;
+            if (count_)
+            {
+                ++(*count_);
+            }
+        }
+        return *this;
+    }
+
     T *operator->() const
     {
         return ptr_;
@@ -46,7 +73,28 @@ private:
     }
 };
 
+class MyClass
+{
+public:
+    MyClass() { std::cout << "MyClass constructor\n"; }
+    ~MyClass() { std::cout << "MyClass ~constructor\n"; }
+    void do_something() { std::cout << "MyClass::do_something() invoked\n"; }
+};
+
 int main()
 {
+    {
+        SharedPtr<MyClass> ptr1(new MyClass());
+        {
+            // SharedPtr<MyClass> ptr2(nullptr);
+            // ptr2 = ptr1;
+            SharedPtr<MyClass> ptr2 = ptr1;
+            ptr1->do_something();
+            ptr2->do_something();
+            std::cout << "count: " << ptr1.use_count() << std::endl;
+        }
+        std::cout << "count: " << ptr1.use_count() << std::endl;
+    }
+
     return 0;
 }
--- shared_ptr/readme.md
+++ shared_ptr/readme.md
@@ -3,4 +3,16 @@
 ### explicit
 explicit的作用就是防止其他类型对该类类型的隐式转换
 
-### 
+### 拷贝构造函数
+```c++
+SharedPtr<MyClass> ptr2(ptr1);
+SharedPtr<MyClass> ptr2 = ptr1;
+```
+https://blog.csdn.net/qq_43519886/article/details/105170209
+
+
+### =运算符重载
+```c++
+SharedPtr<MyClass> ptr2(nullptr);
+ptr2 = ptr1;
+```
